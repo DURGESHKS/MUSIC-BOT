0d7666bca21009932637b09cb36ae16cc3ddb850
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-02-16 15:23:26 +0530

    Update searchResults.js

--- player-events/searchResults.js
+++ player-events/searchResults.js
@@ -4,7 +4,7 @@ module.exports = (client, message, query, tracks) => {
         embed: {
             color: '#304FFE',
             author: { name: `Here are your search results for ${query}` },
-            footer: { text: 'PLAYING BRAWL STARS' },
+            footer: { text: '❃M✯U✯S✯I✯C❃' },
             timestamp: new Date(),
             description: `${tracks.map((t, i) => `**${i + 1}** - ${t.title}`).join('\n')}`,
         },
